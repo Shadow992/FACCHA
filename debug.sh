a50ada236715696dc387eb7606036a68c9362897
C0untLizzi <omit2912@web.de>
2016-08-01 16:42:47 +0200

    Remove automatic execution as we don't have any images yet

--- debug.sh
+++ debug.sh
@@ -3,8 +3,7 @@ cd build
 cmake .. -DDISABLE_WARNINGS=true -DCMAKE_BUILD_TYPE=Debug -G "Unix Makefiles"
 make
 if [ $? -eq 0 ]; then
-    echo "Executing faccha ..."
-	./bin/faccha
+    echo "FACCHA builded successfully"
 else
     echo "Compilation failed ..."
 fi
